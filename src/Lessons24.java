b4c6fc86d298befc44adc7c12a4d61410a292c26
leonidYralsev <yralsevl@gmail.com>
2018-07-17 12:32:59 +0400

    passed the subject: the object class and the toString method()

--- src/Lessons24.java
+++ src/Lessons24.java
@@ -2,4 +2,15 @@
  * Класс Object и метод toString()
  */
 public class Lessons24 {
+    public static void main(String[] args) {
+        String string = "Hello";
+        System.out.println(string);
+        Lessons24 lessons24 = new Lessons24();
+        System.out.println(lessons24.toString());
+    }
+
+    public String toString(){
+        return "Hello";
+    }
+
 }
